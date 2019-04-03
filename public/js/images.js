dfd99d91f185ad69356d9c882569683a7f76051d
tumofreduclips <40761105+tumofreduclips@users.noreply.github.com>
2019-04-03 11:06:48 +0400

    Update images.js

--- public/js/images.js
+++ public/js/images.js
@@ -1,2 +1,2 @@
-const imageList = ["walkingcouple.png","shopdog.png","walkingdog.png","liedown.png","dog.png","run.png","humming.png","pigeon.png","play.png","selfie.png","photo.png"];
+const imageList = ["walkingcouple.png","shopdog.png","walkingdog.png","liedown.png","dog.png","run.png","fly.png","humming.png","pigeon.png","play.png","selfie.png","photo.png"];
 const backgroundList = []
